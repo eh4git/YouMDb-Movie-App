1bad8e7f38a776d94984b98af87aa6c0e94e69e7
Arash Bazrafshan <abazrafshan123@gmail.com>
2020-05-05 14:46:06 -0700

    API calls

--- recommendations.js
+++ recommendations.js
@@ -1,4 +1,5 @@
-var api_key = "8015d3952263abaa7d04e41107994526"
+var api_key1 = "8015d3952263abaa7d04e41107994526"
+var api_key2 = "368220-umdb-OO5NRIBR"
 
 $(".button").on("click", function(){
     var movie = $(".movie-search").val();
@@ -14,16 +15,49 @@ function omdb(searchParam){
     }).then(function(response){
         console.log(response);
         // call movieSearch here
+        
+        var queryDiv = $(".query-result");
+        queryDiv.empty();
+        var title = response.Title;
+        var titleTag = $("<p>").text("Title: " + title);
+        queryDiv.append(titleTag);
+        var year = response.Year;
+        var yearTag = $("<p>").text("Released: " + year);
+        queryDiv.append(yearTag);
+        var rating = response.Rated;
+        var ratingTag = $("<p>").text("Rated: " + rating);
+        queryDiv.append(ratingTag);
+        var genre = response.Genre;
+        var genreTag = $("<p>").text("Genre: " + genre);
+        queryDiv.append(genreTag);
+        var moviePoster = response.Poster;
+        var plot = response.Plot;
+        var plotTag = $("<p>").text("Plot: " + plot);
+        queryDiv.append(plotTag);
+        var image = $("<img>").attr("src", moviePoster);
+        queryDiv.append(image);
+        recommendMovies(title);
     })
-}
 
-function movieSearch(param) {
-    var queryURL2 = "https://api.themoviedb.org/3/movie/550?api_key=" + api_key + "";
+function recommendMovies(searchItem) {
+    var queryURL2 = "https://tastedive.com/api/similar?q=" + title + api_key2;
     $.ajax({
         url: queryURL2,
-        method:"GET"
-    }).then(function(response){
+        method: "GET"
+    }).then(function(repsonse){
         console.log(response);
     })
+}
     
 }
+
+// function movieSearch(param) {
+//     var queryURL2 = "https://api.themoviedb.org/3/movie/550?api_key=" + api_key + "";
+//     $.ajax({
+//         url: queryURL2,
+//         method:"GET"
+//     }).then(function(response){
+//         console.log(response);
+//     })
+    
+// }
