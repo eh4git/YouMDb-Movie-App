ec6fc83dbd510dc82b5e98f89a7d3f471a726501
Tristan Postley <tpostley@gmail.com>
2020-05-06 11:22:53 -0700

    Card Formatting

--- recommendations.js
+++ recommendations.js
@@ -1,9 +1,14 @@
 var api_key1 = "8015d3952263abaa7d04e41107994526"
 var api_key2 = "&k=368220-umdb-OO5NRIBR"
 
-var cardImage = $("#card-image");
-var cardTitle = $(".card-title");
-var cardText = $(".card-text");
+var mainImage = $("#main-image");
+var mainTitle = $("#main-title");
+var mainText = $("#main-text");
+
+var similarImage = $("#similar-image");
+var similarTitle = $("#similar-title");
+var similarText = $("#similar-text");
+
 
 $(".button").on("click", function(){
     var movie = $(".movie-search").val();
@@ -18,50 +23,55 @@ function omdb(searchParam){
         method: "GET"
     }).then(function(response){
         console.log(response);
-        var queryDiv = $(".query-result");
-        queryDiv.empty();
-        var title = response.Title;
-        console.log(title);
-        var titleNoSpace = title.replace(/\s/g, "");
-        console.log(titleNoSpace);
-        var titleTag = $("<p>").text("Title: " + title);
-        queryDiv.append(titleTag);
-        var year = response.Year;
-        var yearTag = $("<p>").text("Released: " + year);
-        queryDiv.append(yearTag);
-        var rating = response.Rated;
-        var ratingTag = $("<p>").text("Rated: " + rating);
-        queryDiv.append(ratingTag);
-        var genre = response.Genre;
-        var genreTag = $("<p>").text("Genre: " + genre);
-        queryDiv.append(genreTag);
-        var moviePoster = response.Poster;
-        var plot = response.Plot;
-        var plotTag = $("<p>").text("Plot: " + plot);
-        queryDiv.append(plotTag);
-        var image = $("<img>").attr("src", moviePoster);
-        queryDiv.append(image);
-        var queryURL2 = "https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=" + title + api_key2;
+
+        mainImage.attr("src", response.Poster);
+        mainTitle.html(response.Title);
+        mainText.html(response.Plot);
+
+        // var queryDiv = $(".query-result");
+        // queryDiv.empty();
+        // var title = response.Title;
+        // // var titleTag = $("<p>").text("Title: " + title);
+        // queryDiv.append(titleTag);
+        // var year = response.Year;
+        // // var yearTag = $("<p>").text("Released: " + year);
+        // queryDiv.append(yearTag);
+        // var rating = response.Rated;
+        // // var ratingTag = $("<p>").text("Rated: " + rating);
+        // queryDiv.append(ratingTag);
+        // var genre = response.Genre;
+        // // var genreTag = $("<p>").text("Genre: " + genre);
+        // queryDiv.append(genreTag);
+        // var moviePoster = response.Poster;
+        // var plot = response.Plot;
+        // // var plotTag = $("<p>").text("Plot: " + plot);
+        // queryDiv.append(plotTag);
+        // // var image = $("<img>").attr("src", moviePoster);
+        // queryDiv.append(image);
+
+        var queryURL2 = "https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=" + response.Title + api_key2;
             $.ajax({
             url: queryURL2,
             method: "GET"
         }).then(function(response){
             console.log(response);
+            
             for (var i = 0; i < 5; i++) {
                 recommendations[i]= response.Similar.Results[i].Name;
                 console.log(recommendations[i]);
                 var queryURL3 = "https://www.omdbapi.com/?t=" + recommendations[i] + "&apikey=trilogy";
+               
                 $.ajax({
                     url: queryURL3,
                     method: "GET"
                 }).then(function(response){
-                 console.log(response);
-                 var moviePoster1 = response.Poster;
-                //  var image1 = $("<img>").attr("src", moviePoster1);
-                //  $(".query-result").prepend(image1);
-                cardImage.attr("src", moviePoster1);
-                cardTitle.html(response.Title);
-                cardText.html(response.Plot);
+                    console.log(response);
+                    var moviePoster1 = response.Poster;
+                    //  var image1 = $("<img>").attr("src", moviePoster1);
+                    //  $(".query-result").prepend(image1);
+                    similarImage.attr("src", moviePoster1);
+                    similarTitle.html(response.Title);
+                    similarText.html(response.Plot);
                 })
             }
         })
